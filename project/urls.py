ba7eccdd84aa096e6336421ceabba11b4b24d2a1
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2025-07-12 11:30:42 +0100

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('tfc.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
